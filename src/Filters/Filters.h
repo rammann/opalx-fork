b29bf90d5ec52b715abd0242ec481389957d93ee
Ryan Ammann <ryanammann24@gmail.com>
2026-06-26 18:34:39 +0200

    Remove dead Enge end-field-model, interpolation cluster, orphans (batch 3-4)
    - EndFieldModel/Enge (factory never populated; setEndFieldModel has no callers)
    - Fields/Interpolation: 18-file dead cluster (only Mesh/NDGrid reachable via DumpEMFields)
    - PBunchDefs.h, TriLinearInterpolator
    Gate exits 0 (214/217, unchanged).

--- src/Filters/Filters.h
+++
@@ -1,9 +0,0 @@
-#ifndef OPALX_FILTERS_HH
-#define OPALX_FILTERS_HH
-
-#include "Filters/FixedFFTLowPass.h"
-#include "Filters/RelativeFFTLowPass.h"
-#include "Filters/SavitzkyGolay.h"
-#include "Filters/Stencil.h"
-
-#endif  // OPALX_FILTERS_HH
